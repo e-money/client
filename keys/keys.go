7d99177af58b426a373393f16d703ec8e89be3b5
Mario Karagiorgas <salem8@gmail.com>
2021-03-16 22:24:28 +0200

    Replace Btc ecdsa prvKey gen

--- keys/keys.go
+++ keys/keys.go
@@ -1,27 +1,17 @@
 package keys
 
 import (
-	"crypto/hmac"
-	"crypto/sha512"
-	"encoding/binary"
-	"encoding/hex"
-	"errors"
-	"fmt"
-	"math/big"
-	"strconv"
-	"strings"
-
-	"github.com/btcsuite/btcd/btcec"
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	cryptotypes "github.com/cosmos/cosmos-sdk/crypto/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
-	"github.com/cosmos/go-bip39"
 )
 
 const (
 	defaultBIP39Passphrase = ""
+	appName                = "e-Money-client"
 )
 
 // KeyManager is an interface for common methods on KeyManagers
@@ -31,26 +21,47 @@ type KeyManager interface {
 	Sign(legacytx.StdSignMsg, *codec.LegacyAmino) ([]byte, error)
 }
 
-// NewMnemonicKeyManager creates a new KeyManager from a mnenomic
-func NewMnemonicKeyManager(mnemonic string) (KeyManager, error) {
-	hdPath := sdk.GetConfig().GetFullFundraiserPath()
-
-	k := keyManager{}
-	err := k.recoveryFromMnemonic(mnemonic, hdPath)
-	return &k, err
-}
-
-// NewPrivateKeyManager creates a new KeyManager from a private key
-func NewPrivateKeyManager(priKey string) (KeyManager, error) {
-	k := keyManager{}
-	err := k.recoveryFromPrivateKey(priKey)
-	return &k, err
-}
-
 type keyManager struct {
+	Account  keyring.Info
 	privKey  cryptotypes.PrivKey
 	addr     sdk.AccAddress
 	mnemonic string
+}
+
+// NewMnemonicKeyManager creates a new KeyManager from a mnenomic
+func NewMnemonicKeyManager(mnemonic string) (KeyManager, error) {
+	hdPath := sdk.FullFundraiserPath
+	prvKey, err := GenPrvKey(mnemonic, defaultBIP39Passphrase, hdPath, hd.Secp256k1)
+	keybase, err := keyring.New(
+		appName, keyring.BackendMemory, hdPath, nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	memInfo, err :=
+		keybase.NewAccount("",
+			mnemonic, defaultBIP39Passphrase, hdPath, hd.Secp256k1)
+	if err != nil {
+		return nil, err
+	}
+
+	k := keyManager{
+		Account:  memInfo,
+		privKey:  prvKey,
+		addr:     memInfo.GetAddress(),
+		mnemonic: mnemonic,
+	}
+	return &k, err
+}
+
+func GenPrvKey(mnemonic, bip39Passwd, hdPath string, algo keyring.SignatureAlgo) (cryptotypes.PrivKey, error) {
+	// create master key and derive first key for keyring
+	derivedPriv, err := hd.Secp256k1.Derive()(mnemonic, bip39Passwd, hdPath)
+	if err != nil {
+		return nil, err
+	}
+
+	return hd.Secp256k1.Generate()(derivedPriv), nil
 }
 
 func (m *keyManager) GetPrivKey() cryptotypes.PrivKey {
@@ -89,143 +100,4 @@ func (m *keyManager) makeSignature(msg legacytx.StdSignMsg) (sig legacytx.StdSig
 		PubKey:    m.privKey.PubKey(),
 		Signature: sigBytes,
 	}, nil
-}
-
-func (m *keyManager) recoveryFromMnemonic(mnemonic, keyPath string) error {
-	words := strings.Split(mnemonic, " ")
-	if len(words) != 12 && len(words) != 24 {
-		return fmt.Errorf("mnemonic length should either be 12 or 24")
-	}
-	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, defaultBIP39Passphrase)
-	if err != nil {
-		return err
-	}
-	// create master key and derive first key:
-	masterPriv, ch := computeMastersFromSeed(seed)
-	derivedPriv, err := derivePrivateKeyForPath(masterPriv, ch, keyPath)
-	if err != nil {
-		return err
-	}
-	secp256k1.GenPrivKey()
-	prvKey := secp256k1.GenPrivKeyFromSecret(derivedPriv[:])
-	addr := sdk.AccAddress(prvKey.PubKey().Address())
-	if err != nil {
-		return err
-	}
-	m.addr = addr
-	m.privKey = prvKey
-	m.mnemonic = mnemonic
-	return nil
-}
-
-// computeMastersFromSeed returns the master public key, master secret, and chain code in hex.
-func computeMastersFromSeed(seed []byte) (secret [32]byte, chainCode [32]byte) {
-	masterSecret := []byte("Bitcoin seed")
-	secret, chainCode = i64(masterSecret, seed)
-
-	return
-}
-
-// derivePrivateKeyForPath derives the private key by following the BIP 32/44 path from privKeyBytes,
-// using the given chainCode.
-func derivePrivateKeyForPath(privKeyBytes [32]byte, chainCode [32]byte, path string) ([32]byte, error) {
-	data := privKeyBytes
-	parts := strings.Split(path, "/")
-	for _, part := range parts {
-		// do we have an apostrophe?
-		harden := part[len(part)-1:] == "'"
-		// harden == private derivation, else public derivation:
-		if harden {
-			part = part[:len(part)-1]
-		}
-		idx, err := strconv.Atoi(part)
-		if err != nil {
-			return [32]byte{}, fmt.Errorf("invalid BIP 32 path: %s", err)
-		}
-		if idx < 0 {
-			return [32]byte{}, errors.New("invalid BIP 32 path: index negative ot too large")
-		}
-		data, chainCode = derivePrivateKey(data, chainCode, uint32(idx), harden)
-	}
-	var derivedKey [32]byte
-	n := copy(derivedKey[:], data[:])
-	if n != 32 || len(data) != 32 {
-		return [32]byte{}, fmt.Errorf("expected a (secp256k1) key of length 32, got length: %v", len(data))
-	}
-
-	return derivedKey, nil
-}
-
-// derivePrivateKey derives the private key with index and chainCode.
-// If harden is true, the derivation is 'hardened'.
-// It returns the new private key and new chain code.
-// For more information on hardened keys see:
-//  - https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki
-func derivePrivateKey(privKeyBytes [32]byte, chainCode [32]byte, index uint32, harden bool) ([32]byte, [32]byte) {
-	var data []byte
-	if harden {
-		index = index | 0x80000000
-		data = append([]byte{byte(0)}, privKeyBytes[:]...)
-	} else {
-		// this can't return an error:
-		_, ecPub := btcec.PrivKeyFromBytes(btcec.S256(), privKeyBytes[:])
-		pubkeyBytes := ecPub.SerializeCompressed()
-		data = pubkeyBytes
-
-		/* By using btcec, we can remove the dependency on tendermint/crypto/secp256k1
-		pubkey := secp256k1.PrivKeySecp256k1(privKeyBytes).PubKey()
-		public := pubkey.(secp256k1.PubKeySecp256k1)
-		data = public[:]
-		*/
-	}
-	data = append(data, uint32ToBytes(index)...)
-	data2, chainCode2 := i64(chainCode[:], data)
-	x := addScalars(privKeyBytes[:], data2[:])
-	return x, chainCode2
-}
-
-func (m *keyManager) recoveryFromPrivateKey(privateKey string) error {
-	priBytes, err := hex.DecodeString(privateKey)
-	if err != nil {
-		return err
-	}
-
-	if len(priBytes) != 32 {
-		return fmt.Errorf("Len of Keybytes is not equal to 32 ")
-	}
-	var keyBytesArray [32]byte
-	copy(keyBytesArray[:], priBytes[:32])
-	priKey := secp256k1.GenPrivKeyFromSecret(keyBytesArray[:])
-	addr := sdk.AccAddress(priKey.PubKey().Address())
-	m.addr = addr
-	m.privKey = priKey
-	return nil
-}
-
-// modular big endian addition
-func addScalars(a []byte, b []byte) [32]byte {
-	aInt := new(big.Int).SetBytes(a)
-	bInt := new(big.Int).SetBytes(b)
-	sInt := new(big.Int).Add(aInt, bInt)
-	x := sInt.Mod(sInt, btcec.S256().N).Bytes()
-	x2 := [32]byte{}
-	copy(x2[32-len(x):], x)
-	return x2
-}
-
-func uint32ToBytes(i uint32) []byte {
-	b := [4]byte{}
-	binary.BigEndian.PutUint32(b[:], i)
-	return b[:]
-}
-
-// i64 returns the two halfs of the SHA512 HMAC of key and data.
-func i64(key []byte, data []byte) (IL [32]byte, IR [32]byte) {
-	mac := hmac.New(sha512.New, key)
-	// sha512 does not err
-	_, _ = mac.Write(data)
-	I := mac.Sum(nil)
-	copy(IL[:], I[:32])
-	copy(IR[:], I[32:])
-	return
 }
--- keys/keys_test.go
+++ keys/keys_test.go
@@ -1,30 +1,34 @@
 package keys
 
 import (
+	"fmt"
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/e-money/em-ledger/types"
 	"github.com/stretchr/testify/require"
-)
-
-const (
-	TestMnenomic     = "play witness auto coast domain win tiny dress glare bamboo rent mule delay exact arctic vacuum laptop hidden siren sudden six tired fragile penalty"
-	TestExpectedAddr = "emoney1lagqmceycrfpkyu7y6ayrk6jyvru5mkrezacpw"
-	TestKavaMnenomic = "equip town gesture square tomorrow volume nephew minute witness beef rich gadget actress egg sing secret pole winter alarm law today check violin uncover"
-	TestKavaCoinID   = 459
 )
 
 func TestNewMnemonicKeyManager(t *testing.T) {
 	types.ConfigureSDK()
 
 	tests := []struct {
-		name       string
-		mnenomic   string
-		coinID     uint32
-		expectpass bool
+		name         string
+		mnenomic     string
+		expectedAddr string
+		expectpass   bool
 	}{
-		{"normal", TestMnenomic, TestKavaCoinID, true},
+		{
+			name:         "first",
+			mnenomic:     "play witness auto coast domain win tiny dress glare bamboo rent mule delay exact arctic vacuum laptop hidden siren sudden six tired fragile penalty",
+			expectedAddr: "emoney1lagqmceycrfpkyu7y6ayrk6jyvru5mkrezacpw",
+			expectpass:   true,
+		},
+		{
+			name:         "second",
+			mnenomic:     "equip town gesture square tomorrow volume nephew minute witness beef rich gadget actress egg sing secret pole winter alarm law today check violin uncover",
+			expectedAddr: "emoney15qdefkmwswysgg4qxgqpqr35k3m49pkxeyrqj5",
+			expectpass:   true,
+		},
 	}
 
 	for _, tc := range tests {
@@ -36,52 +40,8 @@ func TestNewMnemonicKeyManager(t *testing.T) {
 
 				// Confirm correct address
 				addr := keyManager.GetAddr()
-				require.Equal(t, TestExpectedAddr, addr.String())
-			} else {
-				require.NotNil(t, err)
-			}
-		})
-	}
-}
-
-const (
-	Bech32KavaMainPrefix = "kava"
-)
-
-func SetBech32AddressPrefixes(config *sdk.Config) {
-	config.SetBech32PrefixForAccount(Bech32KavaMainPrefix, Bech32KavaMainPrefix+sdk.PrefixPublic)
-	config.SetBech32PrefixForValidator(Bech32KavaMainPrefix+sdk.PrefixValidator+sdk.PrefixOperator, Bech32KavaMainPrefix+sdk.PrefixValidator+sdk.PrefixOperator+sdk.PrefixPublic)
-	config.SetBech32PrefixForConsensusNode(Bech32KavaMainPrefix+sdk.PrefixValidator+sdk.PrefixConsensus, Bech32KavaMainPrefix+sdk.PrefixValidator+sdk.PrefixConsensus+sdk.PrefixPublic)
-}
-
-func TestNewKavaMnemonicKeyManager(t *testing.T) {
-	tests := []struct {
-		name       string
-		mnenomic   string
-		coinID     uint32
-		expected   string
-		expectpass bool
-	}{
-		{
-			name: "normal", mnenomic: TestKavaMnenomic, coinID: TestKavaCoinID,
-			// expected: "kava15qdefkmwswysgg4qxgqpqr35k3m49pkx2jdfnw",
-			expected:   "kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c",
-			expectpass: true,
-		},
-	}
-
-	SetBech32AddressPrefixes(sdk.GetConfig())
-
-	for _, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			keyManager, err := NewKavaMnemonicKeyManager(tc.mnenomic, tc.coinID)
-
-			if tc.expectpass {
-				require.Nil(t, err)
-
-				// Confirm correct address
-				addr := keyManager.GetAddr()
-				require.Equal(t, tc.expected, addr.String())
+				fmt.Println(tc.name, "addr:", addr)
+				require.Equal(t, tc.expectedAddr, addr.String())
 			} else {
 				require.NotNil(t, err)
 			}
